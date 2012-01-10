b0ffdc90032091b8c9fd6022fd8542e25496a1c9
rahul.sampath@gmail.com <rahul.sampath@gmail.com@47602017-6067-e777-203b-c5d0e7fb5a48>
2012-01-10 15:19:28 +0000

    dummy

--- src/schurUtils.C
+++ src/schurUtils.C
@@ -2,3 +2,20 @@
 #include "schur.h"
 #include <cassert>
 
+void createRSDtree(RSDnode *& root, int rank, int npes) {
+  root = new RSDnode;
+  root->rankForCurrLevel = rank;
+  root->npesForCurrLevel = npes;
+  if(npes > 1) {
+    if(rank < (npes/2)) {
+      createRSDtree(root->child, rank, (npes/2));
+    } else {
+      createRSDtree(root->child, (rank - (npes/2)), (npes/2));
+    }
+  } else {
+    root->child = NULL;
+  }
+}
+
+
+
