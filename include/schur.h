d1397505c27c8fb5dd1814cbe668667e92b1c450
rahul.sampath@gmail.com <rahul.sampath@gmail.com@47602017-6067-e777-203b-c5d0e7fb5a48>
2012-01-10 16:22:13 +0000

    dummy

--- include/schur.h
+++ include/schur.h
@@ -3,6 +3,8 @@
 #define __SCHUR__
 
 #include "mpi.h"
+#include "petscmat.h"
+#include "petscdmmg.h"
 
 struct RSDnode {
   RSDnode* child;
@@ -10,7 +12,18 @@ struct RSDnode {
   int npesForCurrLevel;
 };
 
+struct LocalData {
+  MPI_Comm commAll;
+  MPI_Comm commLow, commHigh;
+  Mat Kssl, Kssh;
+  Mat Ksl, Ksh;
+  Mat Kls, Khs;
+  DMMG* mgObj;
+};
+
 void createRSDtree(RSDnode *& root, int rank, int npes);
+
+void createLowAndHighComms(LocalData* data);
 
 #endif
 
--- src/schurUtils.C
+++ src/schurUtils.C
@@ -1,4 +1,5 @@
 
+#include "mpi.h"
 #include "schur.h"
 #include <cassert>
 
@@ -16,6 +17,40 @@ void createRSDtree(RSDnode *& root, int rank, int npes) {
     root->child = NULL;
   }
 }
+
+void createLowAndHighComms(LocalData* data) {
+  int rank, npes;
+  MPI_Comm_rank(data->commAll, &rank);
+  MPI_Comm_size(data->commAll, &npes);
+
+  MPI_Group groupAll;
+  MPI_Comm_group(data->commAll, &groupAll);
+
+  if(rank > 0) {
+    int highRanks[2];
+    highRanks[0] = rank - 1;
+    highRanks[1] = rank;
+    MPI_Group highGroup;
+    MPI_Group_incl(groupAll, 2, highRanks, &highGroup);
+    MPI_Comm_create(data->commAll, highGroup, &(data->commHigh));
+    MPI_Group_free(&highGroup);
+  } else {
+    data->commHigh = MPI_COMM_NULL;
+  }
+
+  if(rank < (npes - 1)) {
+    int lowRanks[2];
+    lowRanks[0] = rank;
+    lowRanks[1] = rank + 1;
+    MPI_Group lowGroup;
+    MPI_Group_incl(groupAll, 2, lowRanks, &lowGroup);
+    MPI_Comm_create(data->commAll, lowGroup, &(data->commLow));
+    MPI_Group_free(&lowGroup);
+  } else {
+    data->commLow = MPI_COMM_NULL;
+  }
+}
+
 
 
 
