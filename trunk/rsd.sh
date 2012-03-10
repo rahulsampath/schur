ac117a9237387ba416c73e7a33d6f5701475c1ac
rahul.sampath@gmail.com <rahul.sampath@gmail.com@47602017-6067-e777-203b-c5d0e7fb5a48>
2012-03-10 16:39:28 +0000

    dummy

---
+++ trunk/rsd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for file in $(find . -type f -name 'rsdN*\.txt')
+do
+ grep "CONVERGED_RTOL" $file | sed s/"Linear solve converged due to CONVERGED_RTOL iterations"/""/ > $file.out
+ export outFileName=`echo $file | sed s/".txt"/".out"/`
+ mv $file.out $outFileName
+ echo processed $file
+done
+
