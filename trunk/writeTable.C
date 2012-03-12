cc913a7675277c1b39ed1d9f0578c504e0a347a1
rahul.sampath@gmail.com <rahul.sampath@gmail.com@47602017-6067-e777-203b-c5d0e7fb5a48>
2012-03-12 17:09:25 +0000

    dummy

--- trunk/writeTable.C
+++ trunk/writeTable.C
@@ -5,14 +5,20 @@
 int main() {
   FILE*fp = fopen("table.tex", "w");
 
-  int N[] = {17, 65, 257};
-  int P[] = {4, 64, 256, 512};
+  //int N[] = {17, 65, 257, 513};
+  //int P[] = {4, 64, 256, 512};
+  int N[] = {513};
+  int P[] = {4, 64, 256};
   int G[] = {1, 2, 4, 8};
 
-  for(int ni = 0; ni < 3; ++ni) {
+  const int Nlen = 1;
+  const int Plen = 3;
+  const int Glen = 4;
+
+  for(int ni = 0; ni < Nlen; ++ni) {
     fprintf(fp, " %d &", (N[ni] - 1));
-    for(int pi = 0; pi < 4; ++pi) {
-      for(int gi = 0; gi < 4; ++gi) {
+    for(int pi = 0; pi < Plen; ++pi) {
+      for(int gi = 0; gi < Glen; ++gi) {
         char fname[256];
         sprintf(fname, "rsdN%dP%dG%d.out", N[ni], P[pi], G[gi]);
         FILE* inp = fopen(fname, "r");
