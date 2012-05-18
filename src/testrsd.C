ccc7fb87142c31a83d1f70122899d25749373e2b
rahul.sampath@gmail.com <rahul.sampath@gmail.com@47602017-6067-e777-203b-c5d0e7fb5a48>
2012-05-18 01:04:44 +0000

    dummy

--- src/testrsd.C
+++ src/testrsd.C
@@ -11,31 +11,20 @@ PetscLogEvent outerKspEvent;
 PetscCookie rsdCookie;
 
 int main(int argc, char** argv) {
-  int debugWait = 0;
-
-  if(argc > 1) {
-    debugWait = atoi(argv[1]);
-  }
-
-  while (debugWait);
-
-  MPI_Init(&argc, &argv);
-
-  int rank, npes;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &npes);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  if(!rank) {
-    std::cout<<"Npes = "<<npes<<std::endl;
-  } 
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  PETSC_COMM_WORLD = MPI_COMM_WORLD;
   PetscInitialize(&argc, &argv, "options", PETSC_NULL);
 
   PetscCookieRegister("RSD", &rsdCookie);
   PetscLogEventRegister("OuterKsp", rsdCookie, &outerKspEvent);
+
+  int rank, npes;
+  MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
+  MPI_Comm_size(PETSC_COMM_WORLD, &npes);
+
+  MPI_Barrier(PETSC_COMM_WORLD);
+  if(!rank) {
+    std::cout<<"Npes = "<<npes<<std::endl;
+  } 
+  MPI_Barrier(PETSC_COMM_WORLD);
 
   int G = 1;
   PetscOptionsGetInt(PETSC_NULL, "-inner_ksp_max_it", &G, PETSC_NULL);
@@ -71,29 +60,27 @@ int main(int argc, char** argv) {
 
   PetscLogEventEnd(outerKspEvent, 0, 0, 0, 0);
 
-  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(PETSC_COMM_WORLD);
   if(!rank) {
     std::cout<<"Done Solve"<<std::endl<<std::endl;
   }
-  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(PETSC_COMM_WORLD);
 
   destroyOuterContext(ctx);
 
-  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(PETSC_COMM_WORLD);
   if(!rank) {
     std::cout<<"Destroyed Outer Context"<<std::endl<<std::endl;
   }
-  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Barrier(PETSC_COMM_WORLD);
+
+  MPI_Barrier(PETSC_COMM_WORLD);
+  if(!rank) {
+    std::cout<<"Done!"<<std::endl<<std::endl;
+  }
+  MPI_Barrier(PETSC_COMM_WORLD);
 
   PetscFinalize();
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  if(!rank) {
-    std::cout<<"Finalized Petsc"<<std::endl<<std::endl;
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  MPI_Finalize();
 
   return 0;
 }
