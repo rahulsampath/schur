04af22efc16dbd5aac9d99055b44fbae8cc514c2
rahul.sampath@gmail.com <rahul.sampath@gmail.com@47602017-6067-e777-203b-c5d0e7fb5a48>
2012-05-21 16:59:31 +0000

    dummy

--- trunk/getIterCnt.sh
+++ trunk/getIterCnt.sh
@@ -2,7 +2,7 @@
 for file in $(find . -type f -name 'rsdN*\.txt')
 do
  grep "CONVERGED_RTOL" $file | sed s/"Linear solve converged due to CONVERGED_RTOL iterations"/""/ > $file.out
- export outFileName=`echo $file | sed s/".txt"/".out"/`
+ export outFileName=`echo $file | sed s/".txt"/"Iter.out"/`
  mv $file.out $outFileName
  echo processed $file
 done
