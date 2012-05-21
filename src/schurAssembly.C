2634d9d6e2f867fb9d6b902e6c56fbab222a1913
rahul.sampath@gmail.com <rahul.sampath@gmail.com@47602017-6067-e777-203b-c5d0e7fb5a48>
2012-05-21 18:00:24 +0000

    dummy

---
+++ src/schurAssembly.C
@@ -0,0 +1,472 @@
+
+#include "schur.h"
+
+extern double stencil[4][4];
+
+PetscErrorCode computeMGmatrix(DMMG dmmg, Mat J, Mat B) {
+  assert(J == B);
+
+  DA da = (DA)(dmmg->dm);
+
+  int N;
+  DAGetInfo(da, PETSC_NULL, &N, PETSC_NULL, PETSC_NULL, 
+      PETSC_NULL, PETSC_NULL, PETSC_NULL, 
+      PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL);
+
+  MatZeroEntries(J);
+
+  int Ne = N - 1;
+  for(int yi = 0; yi < Ne; ++yi) {
+    for(int xi = 0; xi < Ne; ++xi) {
+      int dofs[4];
+      dofs[0] = (yi*N) + xi;
+      dofs[1] = (yi*N) + xi + 1;
+      dofs[2] = ((yi + 1)*N) + xi;
+      dofs[3] = ((yi + 1)*N) + xi + 1;
+      for(int j = 0; j < 4; j++) {
+        for(int i = 0; i < 4; i++) {
+          MatSetValue(J, dofs[j], dofs[i], stencil[j][i], ADD_VALUES);
+        }//end i
+      }//end j
+    }//end xi
+  }//end yi
+
+  MatAssemblyBegin(J, MAT_FLUSH_ASSEMBLY);
+  MatAssemblyEnd(J, MAT_FLUSH_ASSEMBLY);
+
+  //Left
+  for(int yi = 0; yi < N; ++yi) {
+    int xi = 0;
+    int bndDof = (yi*N) + xi;
+    int nhDofs[] = {-1, -1, -1, -1, -1, -1, -1, -1};
+    if(yi > 0) {
+      if(xi > 0) {
+        nhDofs[0] = ((yi - 1)*N) + xi - 1;
+      }
+      nhDofs[1] = ((yi - 1)*N) + xi;
+      if(xi < (N - 1)) {
+        nhDofs[2] = ((yi - 1)*N) + xi + 1;
+      }
+    }
+    if(xi > 0) {
+      nhDofs[3] = (yi*N) + xi - 1;
+    }
+    if(xi < (N - 1)) {
+      nhDofs[4] = (yi*N) + xi + 1;
+    }
+    if(yi < (N - 1)) {
+      if(xi > 0) {
+        nhDofs[5] = ((yi + 1)*N) + xi - 1;
+      }
+      nhDofs[6] = ((yi + 1)*N) + xi;   
+      if(xi < (N - 1)) {
+        nhDofs[7] = ((yi + 1)*N) + xi + 1;
+      }
+    }
+    for(int i = 0; i < 8; ++i) {
+      if(nhDofs[i] != -1) {
+        MatSetValue(J, bndDof, nhDofs[i], 0.0, INSERT_VALUES);
+        MatSetValue(J, nhDofs[i], bndDof, 0.0, INSERT_VALUES);
+      }
+    }//end i
+    MatSetValue(J, bndDof, bndDof, 1.0, INSERT_VALUES);
+  }//end yi
+
+  //Right
+  for(int yi = 0; yi < N; ++yi) {
+    int xi = (N - 1);
+    int bndDof = (yi*N) + xi;
+    int nhDofs[] = {-1, -1, -1, -1, -1, -1, -1, -1};
+    if(yi > 0) {
+      if(xi > 0) {
+        nhDofs[0] = ((yi - 1)*N) + xi - 1;
+      }
+      nhDofs[1] = ((yi - 1)*N) + xi;
+      if(xi < (N - 1)) {
+        nhDofs[2] = ((yi - 1)*N) + xi + 1;
+      }
+    }
+    if(xi > 0) {
+      nhDofs[3] = (yi*N) + xi - 1;
+    }
+    if(xi < (N - 1)) {
+      nhDofs[4] = (yi*N) + xi + 1;
+    }
+    if(yi < (N - 1)) {
+      if(xi > 0) {
+        nhDofs[5] = ((yi + 1)*N) + xi - 1;
+      }
+      nhDofs[6] = ((yi + 1)*N) + xi;   
+      if(xi < (N - 1)) {
+        nhDofs[7] = ((yi + 1)*N) + xi + 1;
+      }
+    }
+    for(int i = 0; i < 8; ++i) {
+      if(nhDofs[i] != -1) {
+        MatSetValue(J, bndDof, nhDofs[i], 0.0, INSERT_VALUES);
+        MatSetValue(J, nhDofs[i], bndDof, 0.0, INSERT_VALUES);
+      }
+    }//end i
+    MatSetValue(J, bndDof, bndDof, 1.0, INSERT_VALUES);
+  }//end yi
+
+  //Top
+  for(int xi = 0; xi < N; ++xi) {
+    int yi = (N - 1);
+    int bndDof = (yi*N) + xi;
+    int nhDofs[] = {-1, -1, -1, -1, -1, -1, -1, -1};
+    if(yi > 0) {
+      if(xi > 0) {
+        nhDofs[0] = ((yi - 1)*N) + xi - 1;
+      }
+      nhDofs[1] = ((yi - 1)*N) + xi;
+      if(xi < (N - 1)) {
+        nhDofs[2] = ((yi - 1)*N) + xi + 1;
+      }
+    }
+    if(xi > 0) {
+      nhDofs[3] = (yi*N) + xi - 1;
+    }
+    if(xi < (N - 1)) {
+      nhDofs[4] = (yi*N) + xi + 1;
+    }
+    if(yi < (N - 1)) {
+      if(xi > 0) {
+        nhDofs[5] = ((yi + 1)*N) + xi - 1;
+      }
+      nhDofs[6] = ((yi + 1)*N) + xi;   
+      if(xi < (N - 1)) {
+        nhDofs[7] = ((yi + 1)*N) + xi + 1;
+      }
+    }
+    for(int i = 0; i < 8; ++i) {
+      if(nhDofs[i] != -1) {
+        MatSetValue(J, bndDof, nhDofs[i], 0.0, INSERT_VALUES);
+        MatSetValue(J, nhDofs[i], bndDof, 0.0, INSERT_VALUES);
+      }
+    }//end i
+    MatSetValue(J, bndDof, bndDof, 1.0, INSERT_VALUES);
+  }//end xi
+
+  //Bottom
+  for(int xi = 0; xi < N; ++xi) {
+    int yi = 0;
+    int bndDof = (yi*N) + xi;
+    int nhDofs[] = {-1, -1, -1, -1, -1, -1, -1, -1};
+    if(yi > 0) {
+      if(xi > 0) {
+        nhDofs[0] = ((yi - 1)*N) + xi - 1;
+      }
+      nhDofs[1] = ((yi - 1)*N) + xi;
+      if(xi < (N - 1)) {
+        nhDofs[2] = ((yi - 1)*N) + xi + 1;
+      }
+    }
+    if(xi > 0) {
+      nhDofs[3] = (yi*N) + xi - 1;
+    }
+    if(xi < (N - 1)) {
+      nhDofs[4] = (yi*N) + xi + 1;
+    }
+    if(yi < (N - 1)) {
+      if(xi > 0) {
+        nhDofs[5] = ((yi + 1)*N) + xi - 1;
+      }
+      nhDofs[6] = ((yi + 1)*N) + xi;   
+      if(xi < (N - 1)) {
+        nhDofs[7] = ((yi + 1)*N) + xi + 1;
+      }
+    }
+    for(int i = 0; i < 8; ++i) {
+      if(nhDofs[i] != -1) {
+        MatSetValue(J, bndDof, nhDofs[i], 0.0, INSERT_VALUES);
+        MatSetValue(J, nhDofs[i], bndDof, 0.0, INSERT_VALUES);
+      }
+    }//end i
+    MatSetValue(J, bndDof, bndDof, 1.0, INSERT_VALUES);
+  }//end xi
+
+  MatAssemblyBegin(J, MAT_FINAL_ASSEMBLY);
+  MatAssemblyEnd(J, MAT_FINAL_ASSEMBLY);
+
+  return 0;
+}
+
+void createLocalMatrices(LocalData* data) {
+  int rank, npes;
+  MPI_Comm_rank(data->commAll, &rank);
+  MPI_Comm_size(data->commAll, &npes);
+
+  int Ne = (data->N) - 1;
+
+  if(rank > 0) {
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Kssh));
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Ksh));
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Khs));
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Khh));
+    MatZeroEntries(data->Kssh);
+    MatZeroEntries(data->Ksh);
+    MatZeroEntries(data->Khs);
+    MatZeroEntries(data->Khh);
+
+    for(int yi = 0; yi < Ne; ++yi) {
+      int dofId[] = {0, 2};
+      int dofs[2];
+      dofs[0] = yi;
+      dofs[1] = yi + 1;
+      for(int j = 0; j < 2; j++) {
+        for(int i = 0; i < 2; i++) {
+          MatSetValue(data->Kssh, dofs[j], dofs[i], stencil[dofId[j]][dofId[i]], ADD_VALUES);
+        }//end i
+      }//end j
+    }//end yi
+
+    MatAssemblyBegin(data->Kssh, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Kssh, MAT_FLUSH_ASSEMBLY);
+
+    MatSetValue(data->Kssh, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssh, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssh, 0, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssh, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssh, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssh, ((data->N) - 1), ((data->N) - 1), 0.0, INSERT_VALUES);
+
+    MatAssemblyBegin(data->Kssh, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Kssh, MAT_FINAL_ASSEMBLY);
+
+    for(int yi = 0; yi < Ne; ++yi) {
+      int sDofId[] = {0, 2};
+      int hDofId[] = {1, 3};
+      int dofs[2];
+      dofs[0] = yi;
+      dofs[1] = yi + 1;
+      for(int si = 0; si < 2; si++) {
+        for(int hi = 0; hi < 2; hi++) {
+          MatSetValue(data->Ksh, dofs[si], dofs[hi], stencil[sDofId[si]][hDofId[hi]], ADD_VALUES);
+          MatSetValue(data->Khs, dofs[hi], dofs[si], stencil[hDofId[hi]][sDofId[si]], ADD_VALUES);
+        }//end hi
+      }//end si
+    }//end yi
+
+    MatAssemblyBegin(data->Ksh, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyBegin(data->Khs, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Ksh, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Khs, MAT_FLUSH_ASSEMBLY);
+
+    MatSetValue(data->Ksh, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksh, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksh, 0, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksh, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksh, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksh, ((data->N) - 1), ((data->N) - 1), 0.0, INSERT_VALUES);
+
+    MatSetValue(data->Khs, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Khs, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Khs, 0, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Khs, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Khs, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Khs, ((data->N) - 1), ((data->N) - 1), 0.0, INSERT_VALUES);
+
+    MatAssemblyBegin(data->Ksh, MAT_FINAL_ASSEMBLY);
+    MatAssemblyBegin(data->Khs, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Ksh, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Khs, MAT_FINAL_ASSEMBLY);
+
+    for(int yi = 0; yi < Ne; ++yi) {
+      int e1DofId[] = {0, 2};
+      int e2DofId[] = {1, 3};
+      int dofs[2];
+      dofs[0] = yi;
+      dofs[1] = yi + 1;
+      for(int j = 0; j < 2; j++) {
+        for(int i = 0; i < 2; i++) {
+          MatSetValue(data->Khh, dofs[j], dofs[i], stencil[e1DofId[j]][e1DofId[i]], ADD_VALUES);
+          MatSetValue(data->Khh, dofs[j], dofs[i], stencil[e2DofId[j]][e2DofId[i]], ADD_VALUES);
+        }//end i
+      }//end j
+    }//end yi
+
+    MatAssemblyBegin(data->Khh, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Khh, MAT_FLUSH_ASSEMBLY);
+
+    MatSetValue(data->Khh, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Khh, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Khh, 0, 0, 1.0, INSERT_VALUES);
+    MatSetValue(data->Khh, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Khh, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Khh, ((data->N) - 1), ((data->N) - 1), 1.0, INSERT_VALUES);
+
+    MatAssemblyBegin(data->Khh, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Khh, MAT_FINAL_ASSEMBLY);
+  } else {
+    data->Kssh = PETSC_NULL;
+    data->Ksh  = PETSC_NULL;
+    data->Khs  = PETSC_NULL;
+    data->Khh  = PETSC_NULL;
+  }
+
+  if(rank < (npes - 1)) {
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Kssl));
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Ksl));
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Kls));
+    MatCreateSeqAIJ(PETSC_COMM_SELF, ((data->N)*(data->dofsPerNode)), ((data->N)*(data->dofsPerNode)),
+        (9*(data->dofsPerNode)), PETSC_NULL, &(data->Kll));
+    MatZeroEntries(data->Kssl);
+    MatZeroEntries(data->Ksl);
+    MatZeroEntries(data->Kls);
+    MatZeroEntries(data->Kll);
+
+    for(int yi = 0; yi < Ne; ++yi) {
+      int dofId[] = {1, 3};
+      int dofs[2];
+      dofs[0] = yi;
+      dofs[1] = yi + 1;
+      for(int j = 0; j < 2; j++) {
+        for(int i = 0; i < 2; i++) {
+          MatSetValue(data->Kssl, dofs[j], dofs[i], stencil[dofId[j]][dofId[i]], ADD_VALUES);
+        }//end i
+      }//end j
+    }//end yi
+
+    MatAssemblyBegin(data->Kssl, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Kssl, MAT_FLUSH_ASSEMBLY);
+
+    MatSetValue(data->Kssl, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssl, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssl, 0, 0, 1.0, INSERT_VALUES);
+    MatSetValue(data->Kssl, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssl, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kssl, ((data->N) - 1), ((data->N) - 1), 1.0, INSERT_VALUES);
+
+    MatAssemblyBegin(data->Kssl, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Kssl, MAT_FINAL_ASSEMBLY);
+
+    for(int yi = 0; yi < Ne; ++yi) {
+      int sDofId[] = {1, 3};
+      int lDofId[] = {0, 2};
+      int dofs[2];
+      dofs[0] = yi;
+      dofs[1] = yi + 1;
+      for(int si = 0; si < 2; si++) {
+        for(int li = 0; li < 2; li++) {
+          MatSetValue(data->Ksl, dofs[si], dofs[li], stencil[sDofId[si]][lDofId[li]], ADD_VALUES);
+          MatSetValue(data->Kls, dofs[li], dofs[si], stencil[lDofId[li]][sDofId[si]], ADD_VALUES);
+        }//end li
+      }//end si
+    }//end yi
+
+    MatAssemblyBegin(data->Ksl, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyBegin(data->Kls, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Ksl, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Kls, MAT_FLUSH_ASSEMBLY);
+
+    MatSetValue(data->Ksl, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksl, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksl, 0, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksl, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksl, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Ksl, ((data->N) - 1), ((data->N) - 1), 0.0, INSERT_VALUES);
+
+    MatSetValue(data->Kls, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kls, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kls, 0, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kls, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kls, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kls, ((data->N) - 1), ((data->N) - 1), 0.0, INSERT_VALUES);
+
+    MatAssemblyBegin(data->Ksl, MAT_FINAL_ASSEMBLY);
+    MatAssemblyBegin(data->Kls, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Ksl, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Kls, MAT_FINAL_ASSEMBLY);
+
+    for(int yi = 0; yi < Ne; ++yi) {
+      int e1DofId[] = {0, 2};
+      int e2DofId[] = {1, 3};
+      int dofs[2];
+      dofs[0] = yi;
+      dofs[1] = yi + 1;
+      for(int j = 0; j < 2; j++) {
+        for(int i = 0; i < 2; i++) {
+          MatSetValue(data->Kll, dofs[j], dofs[i], stencil[e1DofId[j]][e1DofId[i]], ADD_VALUES);
+          MatSetValue(data->Kll, dofs[j], dofs[i], stencil[e2DofId[j]][e2DofId[i]], ADD_VALUES);
+        }//end i
+      }//end j
+    }//end yi
+
+    MatAssemblyBegin(data->Kll, MAT_FLUSH_ASSEMBLY);
+    MatAssemblyEnd(data->Kll, MAT_FLUSH_ASSEMBLY);
+
+    MatSetValue(data->Kll, 0, 1, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kll, 1, 0, 0.0, INSERT_VALUES);
+    MatSetValue(data->Kll, 0, 0, 1.0, INSERT_VALUES);
+    MatSetValue(data->Kll, ((data->N) - 1), ((data->N) - 2), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kll, ((data->N) - 2), ((data->N) - 1), 0.0, INSERT_VALUES);
+    MatSetValue(data->Kll, ((data->N) - 1), ((data->N) - 1), 1.0, INSERT_VALUES);
+
+    MatAssemblyBegin(data->Kll, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(data->Kll, MAT_FINAL_ASSEMBLY);
+  } else {
+    data->Kssl = PETSC_NULL;
+    data->Ksl = PETSC_NULL;
+    data->Kls = PETSC_NULL;
+    data->Kll = PETSC_NULL;
+  }
+}
+
+void zeroBoundary(LocalData* data, Vec vec) {
+  int rank, npes;
+  MPI_Comm_rank(data->commAll, &rank);
+  MPI_Comm_size(data->commAll, &npes);
+
+  PetscScalar* arr;
+  VecGetArray(vec, &arr);
+
+  int oxs, onx;
+  if(rank == 0) {
+    oxs = 0;
+    onx = data->N;
+  } else {
+    oxs = 1;
+    onx = (data->N) - 1;
+  }
+
+  //Left
+  if(rank == 0) {
+    for(int yi = 0; yi < (data->N); ++yi) {
+      int xi = oxs;
+      arr[(yi*onx) + (xi - oxs)] = 0;
+    }//end for yi
+  }
+
+  //Right
+  if(rank == (npes - 1)) {
+    for(int yi = 0; yi < (data->N); ++yi) {
+      int xi = (oxs + onx) - 1;
+      arr[(yi*onx) + (xi - oxs)] = 0;
+    }//end for yi
+  }
+
+  //Top
+  for(int xi = oxs; xi < (oxs + onx); ++xi) {
+    int yi = (data->N) - 1;
+    arr[(yi*onx) + (xi - oxs)] = 0;
+  }//end for xi
+
+  //Bottom
+  for(int xi = oxs; xi < (oxs + onx); ++xi) {
+    int yi = 0;
+    arr[(yi*onx) + (xi - oxs)] = 0;
+  }//end for xi
+
+  VecRestoreArray(vec, &arr);
+}
+
+
+
---
+++ src/schurSetup.C
@@ -0,0 +1,419 @@
+
+#include "schur.h"
+#include <iostream>
+
+void createOuterContext(OuterContext* & ctx) {
+  int rank;
+  int npes;
+  MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
+  MPI_Comm_size(PETSC_COMM_WORLD, &npes);
+
+  ctx = new OuterContext;
+
+  ctx->data = NULL;
+  ctx->root = NULL;
+  ctx->outerMat = PETSC_NULL;
+  ctx->outerKsp = PETSC_NULL;
+  ctx->outerPC = PETSC_NULL;
+  ctx->outerSol = PETSC_NULL;
+  ctx->outerRhs = PETSC_NULL;
+
+  createLocalData(ctx->data);
+
+  createRSDtree(ctx->root, rank, npes);
+
+  createOuterMat(ctx);
+
+  createOuterPC(ctx);
+
+  createOuterKsp(ctx);
+
+  MatGetVecs(ctx->outerMat, &(ctx->outerSol), &(ctx->outerRhs));
+}
+
+void destroyOuterContext(OuterContext* ctx) {
+  if(ctx->outerSol) {
+    VecDestroy(ctx->outerSol);
+  }
+  if(ctx->outerRhs) {
+    VecDestroy(ctx->outerRhs);
+  }
+  if(ctx->outerKsp) {
+    KSPDestroy(ctx->outerKsp);
+  }
+  if(ctx->outerPC) {
+    PCDestroy(ctx->outerPC);
+  }
+  if(ctx->outerMat) {
+    MatDestroy(ctx->outerMat);
+  }
+  if(ctx->root) {
+    destroyRSDtree(ctx->root);
+  }
+  if(ctx->data) {
+    destroyLocalData(ctx->data);
+  }
+  delete ctx;
+}
+
+void createLocalData(LocalData* & data) {
+  data = new LocalData;
+
+  data->commAll = MPI_COMM_NULL;
+  data->commLow = MPI_COMM_NULL;
+  data->commHigh = MPI_COMM_NULL;
+  data->Kssl = PETSC_NULL;
+  data->Kssh = PETSC_NULL;
+  data->Ksl = PETSC_NULL;
+  data->Ksh = PETSC_NULL;
+  data->Kls = PETSC_NULL;
+  data->Khs = PETSC_NULL;
+  data->Kll = PETSC_NULL;
+  data->Khh = PETSC_NULL;
+  data->lowSchurMat = PETSC_NULL;
+  data->highSchurMat = PETSC_NULL;
+  data->lowSchurKsp = PETSC_NULL;
+  data->highSchurKsp = PETSC_NULL;
+  data->mgObj = PETSC_NULL;
+
+  data->dofsPerNode = 1;
+  data->N = 9;
+  PetscOptionsGetInt(PETSC_NULL, "-N", &(data->N), PETSC_NULL);
+  assert((data->N) >= 9);
+  PetscOptionsGetInt(PETSC_NULL, "-D", &(data->dofsPerNode), PETSC_NULL);
+  assert((data->dofsPerNode) >= 1);
+
+  data->commAll = PETSC_COMM_WORLD;
+
+  createLowAndHighComms(data);
+
+  createMG(data);
+
+  createLocalMatrices(data);
+
+  createSchurMat(data);
+
+  createInnerKsp(data);
+}
+
+void destroyLocalData(LocalData* data) {
+  if(data->lowSchurKsp) {
+    KSPDestroy(data->lowSchurKsp);
+  }
+  if(data->highSchurKsp) {
+    KSPDestroy(data->highSchurKsp);
+  }
+  if(data->lowSchurMat) {
+    MatDestroy(data->lowSchurMat);
+  }
+  if(data->highSchurMat) {
+    MatDestroy(data->highSchurMat);
+  }
+  if(data->Kssl) {
+    MatDestroy(data->Kssl);
+  }
+  if(data->Kssh) {
+    MatDestroy(data->Kssh);
+  }
+  if(data->Ksl) {
+    MatDestroy(data->Ksl);
+  }
+  if(data->Ksh) {
+    MatDestroy(data->Ksh);
+  }
+  if(data->Kls) {
+    MatDestroy(data->Kls);
+  }
+  if(data->Khs) {
+    MatDestroy(data->Khs);
+  }
+  if(data->Kll) {
+    MatDestroy(data->Kll);
+  }
+  if(data->Khh) {
+    MatDestroy(data->Khh);
+  }
+  if(data->mgObj) {
+    DMMGDestroy(data->mgObj);
+  }
+  if(data->commLow != MPI_COMM_NULL) {
+    MPI_Comm_free(&(data->commLow));
+  }
+  if(data->commHigh != MPI_COMM_NULL) {
+    MPI_Comm_free(&(data->commHigh));
+  }
+  delete data;
+}
+
+void createRSDtree(RSDnode *& root, int rank, int npes) {
+  root = new RSDnode;
+  root->rankForCurrLevel = rank;
+  root->npesForCurrLevel = npes;
+  if(npes > 1) {
+    if(rank < (npes/2)) {
+      createRSDtree(root->child, rank, (npes/2));
+    } else {
+      createRSDtree(root->child, (rank - (npes/2)), (npes/2));
+    }
+  } else {
+    root->child = NULL;
+  }
+}
+
+void destroyRSDtree(RSDnode *root) {
+  if(root->child) {
+    destroyRSDtree(root->child);
+  }
+  delete root;  
+}
+
+void createLowAndHighComms(LocalData* data) {
+  int rank, npes;
+  MPI_Comm_rank(data->commAll, &rank);
+  MPI_Comm_size(data->commAll, &npes);
+
+  MPI_Group groupAll;
+  MPI_Comm_group(data->commAll, &groupAll);
+
+  if((rank%2) == 0) {
+    if(rank < (npes - 1)) {
+      int lowRanks[2];
+      lowRanks[0] = rank;
+      lowRanks[1] = rank + 1;
+      MPI_Group lowGroup;
+      MPI_Group_incl(groupAll, 2, lowRanks, &lowGroup);
+      MPI_Comm_create(data->commAll, lowGroup, &(data->commLow));
+      MPI_Group_free(&lowGroup);
+    } else {
+      MPI_Comm_create(data->commAll, MPI_GROUP_EMPTY, &(data->commLow));
+      assert((data->commLow) == MPI_COMM_NULL);
+    }
+  } else {
+    int highRanks[2];
+    highRanks[0] = rank - 1;
+    highRanks[1] = rank;
+    MPI_Group highGroup;
+    MPI_Group_incl(groupAll, 2, highRanks, &highGroup);
+    MPI_Comm_create(data->commAll, highGroup, &(data->commHigh));
+    MPI_Group_free(&highGroup);
+  }
+
+  if((rank%2) == 0) {
+    if(rank > 0) {
+      int highRanks[2];
+      highRanks[0] = rank - 1;
+      highRanks[1] = rank;
+      MPI_Group highGroup;
+      MPI_Group_incl(groupAll, 2, highRanks, &highGroup);
+      MPI_Comm_create(data->commAll, highGroup, &(data->commHigh));
+      MPI_Group_free(&highGroup);
+    } else {
+      MPI_Comm_create(data->commAll, MPI_GROUP_EMPTY, &(data->commHigh));
+      assert((data->commHigh) == MPI_COMM_NULL);
+    }
+  } else {
+    if(rank < (npes - 1)) {
+      int lowRanks[2];
+      lowRanks[0] = rank;
+      lowRanks[1] = rank + 1;
+      MPI_Group lowGroup;
+      MPI_Group_incl(groupAll, 2, lowRanks, &lowGroup);
+      MPI_Comm_create(data->commAll, lowGroup, &(data->commLow));
+      MPI_Group_free(&lowGroup);
+    } else {
+      MPI_Comm_create(data->commAll, MPI_GROUP_EMPTY, &(data->commLow));
+      assert((data->commLow) == MPI_COMM_NULL);
+    }
+  }
+
+  MPI_Group_free(&groupAll);
+}
+
+void createOuterKsp(OuterContext* ctx) {
+  KSPCreate((ctx->data)->commAll, &(ctx->outerKsp));
+  PetscObjectIncrementTabLevel((PetscObject)(ctx->outerKsp), PETSC_NULL, 0);
+  KSPSetType(ctx->outerKsp, KSPFGMRES);
+  KSPSetTolerances(ctx->outerKsp, 1.0e-12, 1.0e-12, PETSC_DEFAULT, 50);
+  KSPSetPC(ctx->outerKsp, ctx->outerPC);
+  KSPSetOptionsPrefix(ctx->outerKsp, "outer_");
+  KSPSetFromOptions(ctx->outerKsp);
+  KSPSetOperators(ctx->outerKsp, ctx->outerMat,
+      ctx->outerMat, SAME_NONZERO_PATTERN);
+  KSPSetUp(ctx->outerKsp);
+}
+
+void createOuterMat(OuterContext* ctx) {
+  int rank;
+  MPI_Comm_rank(((ctx->data)->commAll), &rank);
+
+  int onx;
+  if(rank == 0) {
+    onx = (ctx->data)->N;
+  } else {
+    onx = ((ctx->data)->N) - 1;
+  }
+
+  int locSize = onx*((ctx->data)->N)*((ctx->data)->dofsPerNode);
+
+  Mat mat;
+  MatCreateShell(((ctx->data)->commAll), locSize, locSize,
+      PETSC_DETERMINE, PETSC_DETERMINE, ctx, &mat);
+  MatShellSetOperation(mat, MATOP_MULT, (void(*)(void))(&outerMatMult));
+
+  ctx->outerMat = mat;
+}
+
+void createSchurMat(LocalData* data) {
+  int rank, npes;
+  MPI_Comm_rank((data->commAll), &rank);
+  MPI_Comm_size((data->commAll), &npes);
+
+  Mat lowMat = PETSC_NULL;
+  Mat highMat = PETSC_NULL;
+
+  int locSize = (data->N)*(data->dofsPerNode);
+
+  if((rank%2) == 0) {
+    if(rank < (npes - 1)) {
+      MatCreateShell((data->commLow), locSize, locSize,
+          PETSC_DETERMINE, PETSC_DETERMINE, data, &lowMat);
+    }
+  } else {
+    MatCreateShell((data->commHigh), 0, 0,
+        PETSC_DETERMINE, PETSC_DETERMINE, data, &highMat);
+  }
+
+  if((rank%2) == 0) {
+    if(rank > 0) {
+      MatCreateShell((data->commHigh), 0, 0,
+          PETSC_DETERMINE, PETSC_DETERMINE, data, &highMat);
+    }
+  } else {
+    if(rank < (npes - 1)) {
+      MatCreateShell((data->commLow), locSize, locSize,
+          PETSC_DETERMINE, PETSC_DETERMINE, data, &lowMat);
+    }
+  }
+
+  if(lowMat) {
+    MatShellSetOperation(lowMat, MATOP_MULT, (void(*)(void))(&lowSchurMatMult));
+  }
+
+  if(highMat) {
+    MatShellSetOperation(highMat, MATOP_MULT, (void(*)(void))(&highSchurMatMult));
+  }
+
+  data->lowSchurMat = lowMat;
+  data->highSchurMat = highMat;
+}
+
+void createMG(LocalData* data) {
+  assert(data != NULL);
+  int nlevels = 1;
+  PetscOptionsGetInt(PETSC_NULL, "-nlevels", &nlevels, PETSC_NULL);
+  int coarseSize = 1 + (((data->N) - 1)>>(nlevels - 1));
+
+  int rank;
+  MPI_Comm_rank((data->commAll), &rank);
+  if(!rank) {
+    std::cout<<"nlevels = "<<nlevels<<std::endl;
+    std::cout<<"coarseSize = "<<coarseSize<<std::endl;
+  }
+
+  DMMGCreate(PETSC_COMM_SELF, -nlevels, PETSC_NULL, &(data->mgObj));
+  DMMGSetOptionsPrefix(data->mgObj, "loc_");
+
+  DA da;
+  DACreate2d(PETSC_COMM_SELF, DA_NONPERIODIC, DA_STENCIL_BOX, coarseSize, coarseSize,
+      PETSC_DECIDE, PETSC_DECIDE, (data->dofsPerNode), 1, PETSC_NULL, PETSC_NULL, &da);
+  DMMGSetDM((data->mgObj), (DM)da);
+  DADestroy(da);
+
+  DMMGSetKSP((data->mgObj), PETSC_NULL, &computeMGmatrix);
+  PetscObjectIncrementTabLevel((PetscObject)(DMMGGetKSP(data->mgObj)), PETSC_NULL, 2);
+}
+
+void createOuterPC(OuterContext* ctx) {
+  PCCreate(((ctx->data)->commAll), &(ctx->outerPC));
+  PCSetType(ctx->outerPC, PCSHELL);
+  PCShellSetName(ctx->outerPC, "RSD");
+  PCShellSetContext(ctx->outerPC, ctx);
+  PCShellSetApply(ctx->outerPC, &outerPCapply);
+}
+
+void createInnerKsp(LocalData* data) {
+  int rank, npes;
+  MPI_Comm_rank(data->commAll, &rank);
+  MPI_Comm_size(data->commAll, &npes);
+
+  if((rank%2) == 0) {
+    if(rank < (npes - 1)) {
+      KSPCreate(data->commLow, &(data->lowSchurKsp));
+      PetscObjectIncrementTabLevel((PetscObject)(data->lowSchurKsp), PETSC_NULL, 1);
+      KSPSetType(data->lowSchurKsp, KSPFGMRES);
+      KSPSetTolerances(data->lowSchurKsp, 1.0e-12, 1.0e-12, PETSC_DEFAULT, 2);
+      KSPSetOptionsPrefix(data->lowSchurKsp, "inner_");
+      PC pc;
+      KSPGetPC(data->lowSchurKsp, &pc);
+      PCSetType(pc, PCNONE);
+      KSPSetFromOptions(data->lowSchurKsp);
+      KSPSetOperators(data->lowSchurKsp, data->lowSchurMat,
+          data->lowSchurMat, SAME_NONZERO_PATTERN);
+      KSPSetUp(data->lowSchurKsp);
+    } else {
+      data->lowSchurKsp = PETSC_NULL;
+    }
+  } else {
+    KSPCreate(data->commHigh, &(data->highSchurKsp));
+    PetscObjectIncrementTabLevel((PetscObject)(data->highSchurKsp), PETSC_NULL, 1);
+    KSPSetType(data->highSchurKsp, KSPFGMRES);
+    KSPSetTolerances(data->highSchurKsp, 1.0e-12, 1.0e-12, PETSC_DEFAULT, 2);
+    KSPSetOptionsPrefix(data->highSchurKsp, "inner_");
+    PC pc;
+    KSPGetPC(data->highSchurKsp, &pc);
+    PCSetType(pc, PCNONE);
+    KSPSetFromOptions(data->highSchurKsp);
+    KSPSetOperators(data->highSchurKsp, data->highSchurMat,
+        data->highSchurMat, SAME_NONZERO_PATTERN);
+    KSPSetUp(data->highSchurKsp);
+  }
+
+  if((rank%2) == 0) {
+    if(rank > 0) {
+      KSPCreate(data->commHigh, &(data->highSchurKsp));
+      PetscObjectIncrementTabLevel((PetscObject)(data->highSchurKsp), PETSC_NULL, 1);
+      KSPSetType(data->highSchurKsp, KSPFGMRES);
+      KSPSetTolerances(data->highSchurKsp, 1.0e-12, 1.0e-12, PETSC_DEFAULT, 2);
+      KSPSetOptionsPrefix(data->highSchurKsp, "inner_");
+      PC pc;
+      KSPGetPC(data->highSchurKsp, &pc);
+      PCSetType(pc, PCNONE);
+      KSPSetFromOptions(data->highSchurKsp);
+      KSPSetOperators(data->highSchurKsp, data->highSchurMat,
+          data->highSchurMat, SAME_NONZERO_PATTERN);
+      KSPSetUp(data->highSchurKsp);
+    } else {
+      data->highSchurKsp = PETSC_NULL;
+    }
+  } else {
+    if(rank < (npes - 1)) {
+      KSPCreate(data->commLow, &(data->lowSchurKsp));
+      PetscObjectIncrementTabLevel((PetscObject)(data->lowSchurKsp), PETSC_NULL, 1);
+      KSPSetType(data->lowSchurKsp, KSPFGMRES);
+      KSPSetTolerances(data->lowSchurKsp, 1.0e-12, 1.0e-12, PETSC_DEFAULT, 2);
+      KSPSetOptionsPrefix(data->lowSchurKsp, "inner_");
+      PC pc;
+      KSPGetPC(data->lowSchurKsp, &pc);
+      PCSetType(pc, PCNONE);
+      KSPSetFromOptions(data->lowSchurKsp);
+      KSPSetOperators(data->lowSchurKsp, data->lowSchurMat,
+          data->lowSchurMat, SAME_NONZERO_PATTERN);
+      KSPSetUp(data->lowSchurKsp);
+    } else {
+      data->lowSchurKsp = PETSC_NULL;
+    }
+  }
+}
+
+
+
